0b9219eabbed3df372050b1167860e50319dd10b
Geoff Davis <geoffdavis92@gmail.com>
2018-04-04 04:40:29 -0500

    get storybook working:

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    ecommerceListing: "src/ecommerce.jsx"
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/")
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+    alias: {
+			EcommerceListing: path.resolve(__dirname, 'src/ecommerce-listing/'),
+      types: path.resolve(__dirname, "src/types"),
+      utils: path.resolve(__dirname, "src/utils")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
