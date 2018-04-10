2c7d0a3895c88c9e8581f341b2317c5a5b48087e
Geoff Davis <geoffdavis92@gmail.com>
2018-04-09 21:41:55 -0500

    add aliases for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,8 +5,14 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const path =  require('path')
 
 module.exports = {
+  resolve: {
+    alias: {
+      EcommerceListing: path.resolve(__dirname,'../src/ecommerce-listing/index')
+    }
+  },
   plugins: [
     // your custom plugins
   ],
