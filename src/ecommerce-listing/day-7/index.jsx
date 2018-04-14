06089d95b8331f759d3212d0a02fabc5bb340c75
Geoff Davis <geoffdavis92@gmail.com>
2018-04-14 15:55:10 -0500

    add day 7

---
+++ src/ecommerce-listing/day-7/index.jsx
@@ -0,0 +1,338 @@
+import React from "react";
+import styled from "styled-components";
+import { faHeart } from "@fortawesome/fontawesome-free-regular";
+import {
+  faCartArrowDown,
+  faCartPlus,
+  faHeart as faHeartSolid,
+  faTrashAlt
+} from "@fortawesome/fontawesome-free-solid";
+import FA from "@fortawesome/react-fontawesome";
+import { THEME } from "EcommerceListing/util";
+
+const ListingContainer = styled.article`
+  border: 1px solid #eee;
+  border-radius: 2px;
+  display: inline-block;
+  font-family: ${THEME.fontFamily};
+  font-size: 20px;
+  margin: 0.5em;
+  padding: 1em;
+  max-width: 300px;
+`;
+
+const ListingImage = styled.figure.attrs({ role: "group" })`
+  margin: 0 auto 1em;
+  position: relative;
+  img {
+    border-radius: 2px;
+    display: block;
+    max-width: 100%;
+  }
+`;
+
+const ListingTitle = styled.h3`
+  font-size: 1.2em;
+  margin: 0 auto 0.5em;
+`;
+
+const ListingSubtitle = styled.h4`
+  font-weight: 400;
+  margin: 0 auto 1em;
+  text-transform: capitalize;
+`;
+
+const PriceDisplay = styled.h5`
+  font-size: 1.2em;
+  height: 1.4em;
+  line-height: 1;
+  margin: 0 auto 0.85em;
+  text-align: right;
+  vertical-align: middle;
+`;
+
+const FavButton = styled.button`
+  background: transparent;
+  border: none;
+  color: ${props => (props.isFavorite ? THEME.red : "#fff")};
+  cursor: pointer;
+  font-size: 1.2em;
+  line-height: 1;
+  padding: 0;
+  position: absolute;
+  top: 0.5em;
+  right: 0.5em;
+`;
+
+const CartButton = styled.button`
+  background-color: ${props =>
+    props.disabled
+      ? THEME.gray
+      : !props.showAddedToCart && props.inCart && props.hover
+        ? THEME.red
+        : props.inCart
+          ? THEME.green
+          : THEME.blue};
+  border: none;
+  border-radius: 2px;
+  color: ${props => (props.disabled ? THEME.grayDark : "#fff")};
+  cursor: ${props =>
+    props.disabled
+      ? "not-allowed"
+      : props.hover && props.showAddedToCart
+        ? "default"
+        : "pointer"};
+  display: block;
+  font-family: ${THEME.fontFamily};
+  font-size: 0.9em;
+  padding: 0.5em;
+  width: 100%;
+`;
+
+const Badge = styled.span`
+  border: 2px solid
+    ${props => (props.soldOut ? THEME.red : props.sale ? THEME.green : "none")};
+  border-radius: 2px;
+  color: ${props =>
+    props.soldOut ? THEME.red : props.sale ? THEME.green : "inherit"};
+  display: inline-block;
+  font-size: 0.75em;
+  line-height: 1;
+  padding: 0.33em 0.5em;
+  vertical-align: top;
+`;
+
+const Sale = styled.span`
+  color: ${THEME.green};
+  display: inline-block;
+  margin: 0 0.5em;
+  vertical-align: sub;
+`;
+
+const Strikethrough = styled.span`
+  color: ${THEME.grayDark};
+  text-decoration: line-through;
+  vertical-align: sub;
+`;
+
+const ListingPrice = ({ children, sale, soldOut }) => {
+  const Price = soldOut ? (
+    <Badge soldOut>SOLD OUT</Badge>
+  ) : sale ? (
+    <React.Fragment>
+      <Badge sale>SALE</Badge>
+      <Sale>{sale}</Sale>
+      <Strikethrough>{children}</Strikethrough>
+    </React.Fragment>
+  ) : (
+    children
+  );
+  return <PriceDisplay>{Price}</PriceDisplay>;
+};
+
+const VariantWrapper = styled.span`
+  display: block;
+  margin: 1em 0;
+`;
+
+const VariantInlineBlock = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const VariantGrid = styled.span`
+  display: flex;
+  @supports (display: grid) {
+    display: grid;
+    grid-template-columns: repeat(4, 40px);
+    grid-column-gap: 0.5em;
+    grid-row-gap: 0.5em;
+  }
+`;
+
+const VariantImage = styled.span`
+  cursor: ${props => (props.disableSelection ? "not-allowed" : "default")};
+  display: inline-block;
+  width: 2em;
+  img {
+    border: ${props => (props.selected ? `.25em solid ${THEME.blue}` : "none")};
+    border-radius: 2px;
+    display: block;
+    opacity: ${props => (props.selected ? 1 : 0.5)};
+    max-width: ${props => (props.selected ? `calc(100% - .5em)` : "100%")};
+  }
+`;
+
+const getAltTextFromSrc = src => {
+  const pattern = /.+\-([a-z]+)+.[a-z]+$/;
+  const [_, productColor] = pattern.exec(src);
+  return productColor;
+};
+
+const ListingVariants = ({
+  children,
+  onClick,
+  disableSelection,
+  selectedVariant
+}) => {
+  return (
+    <VariantWrapper>
+      <VariantInlineBlock>Colors:&nbsp;</VariantInlineBlock>
+      <VariantInlineBlock>
+        <VariantGrid>
+          {React.Children.map(children, (child, index) => (
+            <VariantImage
+              key={index}
+              selected={index === selectedVariant}
+              onClick={e =>
+                !disableSelection ? onClick({ index, imgsrc: child }) : null
+              }
+              {...{ disableSelection }}
+            >
+              <img src={child} alt={getAltTextFromSrc(child)} />
+            </VariantImage>
+          ))}
+        </VariantGrid>
+      </VariantInlineBlock>
+    </VariantWrapper>
+  );
+};
+
+class ListingCartButton extends React.Component {
+  state = { hover: false };
+  _toggleHoverState = e => {
+    this.setState(prevState => ({
+      hover: !prevState.hover,
+      showAddedToCart: false
+    }));
+  };
+  render() {
+    const { disabled, inCart, onClick } = this.props;
+    const { hover, showAddedToCart } = this.state;
+    const ButtonContent =
+      inCart && showAddedToCart ? (
+        <React.Fragment>
+          <FA icon={faCartArrowDown} aria-label="in-cart icon" /> ADDED TO CART
+        </React.Fragment>
+      ) : inCart && hover ? (
+        <React.Fragment>
+          <FA icon={faTrashAlt} aria-label="remove-from-cart icon" /> REMOVE
+          FROM CART
+        </React.Fragment>
+      ) : !inCart ? (
+        <React.Fragment>
+          <FA icon={faCartPlus} aria-label="add-to-cart icon" /> ADD TO CART
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <FA icon={faCartArrowDown} aria-label="in-cart icon" /> ADDED TO CART
+        </React.Fragment>
+      );
+    return (
+      <CartButton
+        hover={hover}
+        type="button"
+        onClick={e => {
+          hover &&
+            !showAddedToCart &&
+            this.setState(
+              prevState => ({
+                showAddedToCart: inCart ? false : true
+              }),
+              () => onClick()
+            );
+        }}
+        {...{ disabled, inCart, showAddedToCart }}
+        onMouseEnter={() => (!disabled ? this._toggleHoverState() : null)}
+        onMouseLeave={() => (!disabled ? this._toggleHoverState() : null)}
+      >
+        {ButtonContent}
+      </CartButton>
+    );
+  }
+}
+
+export default class EcommerceListing extends React.Component {
+  state = {
+    listingImageSrc:
+      "https://raw.githubusercontent.com/geoffdavis92/weekly-ui/master/assets/ecommerce-listing/thinsulate-hat-orange.jpg",
+    inCart: false,
+    isFavorite: false,
+    selectedVariant: 0
+  };
+  _selectVariant = ({ index, imgsrc }) => {
+    this.setState(prevState => ({
+      listingImageSrc: imgsrc,
+      selectedVariant: index
+    }));
+  };
+  _toggleFavorite = () => {
+    this.setState(prevState => ({
+      isFavorite: !prevState.isFavorite
+    }));
+  };
+  _toggleInCart = () => {
+    this.setState(prevState => ({
+      inCart: !prevState.inCart
+    }));
+  };
+  render() {
+    const { sale, soldOut } = this.props;
+    // const pattern = /.+\-([a-z]+)+.[a-z]+$/;
+    // const [_, productColor] = pattern.exec(this.state.listingImageSrc);
+    const productColor = getAltTextFromSrc(this.state.listingImageSrc);
+    return (
+      <React.Fragment>
+        <style>
+          /* import fonts for display */ @import
+          url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+        </style>
+        <ListingContainer>
+          <ListingImage>
+            <FavButton
+              aria-label="favorite button"
+              type="button"
+              isFavorite={this.state.isFavorite}
+              onClick={this._toggleFavorite}
+            >
+              <FA
+                icon={this.state.isFavorite ? faHeartSolid : faHeart}
+                aria-label={
+                  this.state.isFavorite ? "favorited icon" : "add-favorite icon"
+                }
+              />
+            </FavButton>
+            <img
+              src={this.state.listingImageSrc}
+              alt={`Thinsulate knitted winter cap in ${productColor}`}
+            />
+          </ListingImage>
+          <header>
+            <ListingTitle>Thinsulate Winter Cap</ListingTitle>
+            <ListingSubtitle>{productColor}</ListingSubtitle>
+          </header>
+          <ListingPrice {...{ sale, soldOut }}>$34.99</ListingPrice>
+          <ListingVariants
+            disableSelection={this.state.inCart}
+            selectedVariant={this.state.selectedVariant}
+            onClick={!this.state.inCart ? this._selectVariant : null}
+          >
+            {[
+              "https://raw.githubusercontent.com/geoffdavis92/weekly-ui/master/assets/ecommerce-listing/thinsulate-hat-orange.jpg",
+              "https://raw.githubusercontent.com/geoffdavis92/weekly-ui/master/assets/ecommerce-listing/thinsulate-hat-blue.jpg",
+              "https://raw.githubusercontent.com/geoffdavis92/weekly-ui/master/assets/ecommerce-listing/thinsulate-hat-gray.jpg",
+              "https://raw.githubusercontent.com/geoffdavis92/weekly-ui/master/assets/ecommerce-listing/thinsulate-hat-yellow.jpg"
+            ]}
+          </ListingVariants>
+          <footer>
+            <ListingCartButton
+              disabled={soldOut}
+              inCart={this.state.inCart}
+              onClick={this._toggleInCart}
+            />
+          </footer>
+        </ListingContainer>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/ecommerce-listing/day-7/post.md
@@ -0,0 +1,37 @@
+---
+cover_image: https://thepracticaldev.s3.amazonaws.com/i/njqzjs9z01djgckfv8de.jpg
+title: "Weekly UI Challenge Week 1 Day 7: Tweaks, refactors, fixes"
+published: false
+description: "Week 1 Day 7 of my Weekly UI Challenge: All the fixings"
+tags: ui,weeklyui,challenge,react
+---
+
+## A quick recap
+
+As we wind down this week's challenge, I want to thank **[Ben](https://dev.to/ben)** and his fellow **dev.to admins** for getting behind this Weekly UI Challenge and setting up some great new tools for moderating the [#weeklyui](https://dev.to/t/weeklyui) tag page. (seriously, check it out, it looks awesome)
+
+Thank you to everyone who has participated, retweeted, and liked/unicorn'd these posts; I really hope we can get more of the community involved for Week 2 and beyond. Speaking of participants, **[Ali Spittel](https://dev.to/aspittel)** gets the Week 1 Gold Star🌟 for tweeting and displaying her daily results!
+
+---
+
+Welcome to Week 1, Day 7 of my Weekly UI challenge! As I stated in the [announcement post](https://dev.to/geoff/announcing-weekly-ui-challenge-h87), week 1 will focus on an **ecommerce listing** UI component; each day throughout this following week, I will pick one or two (usually related) subelements of the design to implement. For day seven, our goal is to…
+
+## Tweak, refactor, and/or fix your code
+
+Today is all about you and your design/code perfectionism. Did you want to change your component API to be more flexible? Did you mean to tweak spacing on Day 5 but didn't have time? Perhaps you needed more time to research how to fix a few more a11y issues before you get that perfect score on Day 6. We all have to get sleep, spend time with friends and family, and have other things pop up that may affect your workflow; today is your day to finish strong and get everything up to spec as *you* see fit.
+
+I will be updating my variant thumbnails to be tab-able, adjusting focus states on my buttons, and updating the API to be more programmatic. As always, you can check out my coded implementation [on my Github pages site for this project](https://geoffdavis92.github.io/weekly-ui/).
+
+Please add your repos and/or images of your designs in the comments for inspiration! I would love to see what you all created throughout the week.
+
+Happy fixing! 🎉
+
+### Week 1 Calendar
+
+1. Design component ✅
+2. Display product name, price, and image ✅
+3. Add to cart button, favorite button ✅
+4. Sale price display, sold out states ✅
+5. Color variant thumbnail buttons ✅
+6. 100% a11y score ✅
+7. Tweaks, refactors, fixes 🎯
